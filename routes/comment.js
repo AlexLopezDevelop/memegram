2f2f3fba07984111643508c8ab250450f2856bec
AlexLopezDevelop <alex_lopez.es@icloud.com>
2022-04-24 19:02:26 +0200

    added: basic auth form some endpoints

--- routes/comment.js
+++ routes/comment.js
@@ -1,8 +1,9 @@
 const { getTotalUserComments, addComment, editComment, deleteComment, getAllCommentsByPost, getTotalCommentsPost } = require("./controllers/comment");
+const { privateRoute } = require("../middlewares/privateRoute");
 const commentSettings = require('express').Router({ mergeParams: true });
 
 commentSettings.get("/total/user", getTotalUserComments)
-commentSettings.post("/add", addComment)
+commentSettings.post("/add", privateRoute, addComment)
 commentSettings.post("/edit", editComment)
 commentSettings.delete("/delete", deleteComment)
 commentSettings.get("/all", getAllCommentsByPost)
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,12 @@
 const { getAllPosts, getAllUserPosts, getPostById, getTotalUserPosts, addPost, editPost, deletePost } = require("./controllers/post");
+const { privateRoute } = require("../middlewares/privateRoute");
 const postSettings = require('express').Router({ mergeParams: true });
 
 postSettings.get("/all", getAllPosts)
 postSettings.get("/all/user", getAllUserPosts)
 postSettings.get("/", getPostById)
 postSettings.get("/total/user", getTotalUserPosts)
-postSettings.post("/add", addPost)
+postSettings.post("/add", privateRoute, addPost)
 postSettings.post("/edit", editPost)
 postSettings.delete("/delete", deletePost)
 
