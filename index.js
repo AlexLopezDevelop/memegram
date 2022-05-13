1399f89b07360104780dd3abc0ff2a632be900ac
AlexLopezDevelop <alex_lopez.es@icloud.com>
2022-05-14 00:52:43 +0200

    implementing: swagger

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const memegramRouter = require('./routes/index.js');
 const app = express();
-const { privateRoute } = require('./middlewares/privateRoute.js')
-const port = 3000
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
+const port = 3000
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Memegram API',
+      description: 'Memegram API',
+      contact: {
+        name: 'Alex Lopez',
+      },
+      servers: [`http://localhost:${port}`],
+    },
+  },
+  apis: ['./routes/*.js'],
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(memegramRouter);
 app.use(express.json());
 
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,45 @@ const { getAllUsers, getUserById, addUser, editUser, deleteUser, loginUser } = r
 const { privateRoute } = require("../middlewares/privateRoute");
 const userSettings = require('express').Router({ mergeParams: true });
 
-userSettings.get("/all", privateRoute, getAllUsers)
+/**
+ * @swagger
+ * tags:
+ *  name: User
+ *  description: User management
+ */
+
+/**
+ * @swagger
+ * /{id}:
+ *  get:
+ *    summary: Get user by id
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: number
+ *        required: true
+ *        description: The user id
+ *    responses:
+ *      200:
+ *        description: Database response
+ *      404:
+ *        description: User not found
+ */
 userSettings.get("/", privateRoute, getUserById)
+
+/**
+ * @swagger
+ * /all:
+ *  get:
+ *    description: Get all users
+ *    tags: [User]
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+userSettings.get("/all", privateRoute, getAllUsers)
 userSettings.post("/add", addUser)
 userSettings.post("/edit", privateRoute, editUser)
 userSettings.delete("/delete", privateRoute, deleteUser)
